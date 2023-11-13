16d516e0bca5f86dac144dcbbb6eb6f52679f0f7
luise-phd <luis.ordonez.palacios@correounivalle.edu.co>
2023-11-12 19:01:53 -0500

    Renderiza todos los usuarios si es admin

--- components/ListaUsuarios.js
+++ components/ListaUsuarios.js
@@ -92,17 +92,18 @@ const ListaUsuarios = ({ admin }) => {
         }
       );
 
-      if(admin === "Si" || adminAsync === "Si") {
+      // if(admin === "Si" || adminAsync === "Si") {
+      //   const usuarios = respuesta2.data.usuarios.filter((usuario) => {
+      //     return usuario._id !== id;
+      //   });
+      //   setUsuarios(usuarios);
+      // } else {
         const usuarios = respuesta2.data.usuarios.filter((usuario) => {
+          // return usuario.state === "Activo" && usuario._id !== id;
           return usuario._id !== id;
         });
         setUsuarios(usuarios);
-      } else {
-        const usuariosActivos = respuesta2.data.usuarios.filter((usuario) => {
-          return usuario.state === "Activo" && usuario._id !== id;
-        });
-        setUsuarios(usuariosActivos);
-      }
+      // }
     } catch (error) {
       console.log(error);
     }
@@ -191,17 +192,21 @@ const ListaUsuarios = ({ admin }) => {
   };
 
   const renderItem = ({ item }) => (
-    <TouchableOpacity style={[styles.item, { width: deviceWidth }]}
-      onLongPress={() => handleLongPress(item)}
-      onPress={() => handleItemClick(item)}>
-      <View style={styles.leftColumn}>
-        <Text style={{ fontSize: 16, color: item.state === 'Inactivo' ? 'lightgray' : 'black' }}>{item.nombre}</Text>
-        <Text style={{ fontSize: 14, color: item.state === 'Inactivo' ? 'lightgray' : 'black' }}>{item.phone}</Text>
-      </View>
-      <Text style={{ fontSize: 14, color: item.state === 'Inactivo' ? 'lightgray' : 'black' }}>
-        {item.mensajesSinLeer > 0 ? item.mensajesSinLeer: ""}
-      </Text>
-    </TouchableOpacity>
+    <>
+      {admin === 'Si' || (admin === 'No' && item.state === 'Activo') ? (
+        <TouchableOpacity style={[styles.item, { width: deviceWidth }]}
+          onLongPress={() => handleLongPress(item)}
+          onPress={() => handleItemClick(item)}>
+          <View style={styles.leftColumn}>
+            <Text style={{ fontSize: 16, color: item.state === 'Inactivo' ? 'lightgray' : 'black' }}>{item.nombre}</Text>
+            <Text style={{ fontSize: 14, color: item.state === 'Inactivo' ? 'lightgray' : 'black' }}>{item.phone}</Text>
+          </View>
+          <Text style={{ fontSize: 14, color: item.state === 'Inactivo' ? 'lightgray' : 'black' }}>
+            {item.mensajesSinLeer > 0 ? item.mensajesSinLeer : ""}
+          </Text>
+        </TouchableOpacity>
+      ) : null}
+    </>
   );
 
   const deviceWidth = Dimensions.get('window').width - 10;
